444130d23bcbb3fa5512b8bd302f141b552c8760
Imani A <imartin@miners.utep.edu>
2019-09-01 17:46:11 -0600

    excluding white space and punctuation

--- wordCount.py
+++ wordCount.py
@@ -1,13 +1,23 @@
 #! /usr/bin/env python3
 import sys
 import os
+import re
 
 # setting cmdLine arguments for file names
 textFile = sys.argv[1]
-inputFile = sys.argv[2]
-outputFile = sys.argv[3]
+outputFile = sys.argv[2]
 
+readText = open(textFile).read()
+
+wordList = readText.split()
+wordSet = set(wordList)
+
+# create output file
 if not os.path.exists(outputFile):
-    with open('outputFile', 'w'): pass
+    with open(outputFile, 'w'): pass
 
+for w in wordSet:
+    w = re.sub(r"[^a-z0-9]","", w.lower())
+
+    print(w, ": ", wordList.count(w))
 
